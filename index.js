5485769e9178c5d9e496fef8267e96644fe4c272
Rick Van <x.rckvan@gmail.com>
2017-07-25 20:09:40 -0600

    added file name variable and created html file from req body

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-let request = require('request');
+const fs = require('fs');
+const request = require('request');
+
 let url = process.argv[2];
+let fileName = process.argv[3]
 
 request(url, (error, response, body) => {
-  console.log('error:', error);
-  console.log('statusCode:', response && response.statusCode);
-  console.log('body:', body);
+  if (error) return console.error(error);
+  if (response && response.statusCode == 200) {
+    fs.writeFile(fileName, body, (error)=>{
+      if (error) return console.error(error);
+    })
+  }
 });
